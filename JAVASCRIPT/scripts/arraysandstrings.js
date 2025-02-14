c7abc28daf25e479497a5cde9f1065239a3439f0
vinaneosoft <vinaneosoft@gmail.com>
2025-02-14 12:55:10 +0530

    commit

--- JAVASCRIPT/scripts/arraysandstrings.js
+++ JAVASCRIPT/scripts/arraysandstrings.js
@@ -21,4 +21,72 @@ let arr7=new Array('hi'); //length=1
 
 //splice,includes,join,sort,reverse
 
+arr5.splice(1,1); /*startIndex, deleteCount */
+console.log(arr5);
+
+console.log( arr3.includes('vina'));
+
+let numbers = [220, 20,100, 10, 34, 344, 3];
+
+numbers.sort((n1,n2)=>{
+    console.log(n1+" "+n2);
+    return n1-n2;
+    }
+);
+numbers.sort((n1,n2)=>n1-n2);
+console.log(numbers);
+
+
+console.log(numbers.includes(34));
+
+numbers.reverse(); // mutable
+console.log(numbers);
+// self study : implement other array API functions
+
+
+let s1="";  // primitive
+let s2="Hello";
+console.log(typeof s2);
+
+let s3=new String();
+let s4=new String("India is my country"); // object of type String
+console.log(typeof s4);
+console.log(s4 instanceof String);
+
+// string are always immutable, no change in value
+
+let upperstring=s4.toUpperCase();
+console.log(s4);
+console.log(upperstring);
+
+let city="Solapur";
+city=city.concat(" Kolhapur"); // ref used same to point to new memory location
+console.log(city); 
+// we want avoid above
+
+const a=89;
+/* a=90;  */// error
+
+
+const company="neosoft";
+/* company=company+" rabale" */;
+const concated=company+" rabale"
+console.log(concated);
+console.log(company);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
