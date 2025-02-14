8352c16507152dc5e498f56db84787f4a442d70b
vinaneosoft <vinaneosoft@gmail.com>
2025-02-14 12:13:46 +0530

    commit

--- JAVASCRIPT/scripts/arraysandstrings.js
+++ JAVASCRIPT/scripts/arraysandstrings.js
@@ -7,7 +7,7 @@ let arr3=['vina','veda'];
 arr1[2]=77; // length increased
 arr1[0]=34; // replace
 console.log(arr1);
-const length=arr1.push(89); // add new values from back
+let length=arr1.push(89); // add new values from back
 console.log(arr1);
 length=arr1.unshift(11); // add new values from front
 console.log(arr1);
@@ -19,6 +19,6 @@ let arr5=new Array(78,66,444); //length=3
 let arr6=new Array(5); // length=5
 let arr7=new Array('hi'); //length=1
 
-splice,includes,join,sort,reverse
+//splice,includes,join,sort,reverse
 
 
