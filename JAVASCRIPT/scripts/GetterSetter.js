46956487c050359337df8e473805f188a0bb78a3
vinaneosoft <vinaneosoft@gmail.com>
2025-02-19 12:00:24 +0530

    commit

---
+++ JAVASCRIPT/scripts/GetterSetter.js
@@ -0,0 +1,63 @@
+
+class BankAccount{
+    #accountNumber;
+    accountType;
+    accountBalance;
+    customerId;
+    constructor(type='savings', accbal=0,cid='SC0',accnum=0){
+       this.accountBalance=accbal;
+       this.#accountNumber=accnum;
+       this.accountType=type;
+       this.customerId=cid;
+    }
+    deposit(amount){
+        this.accountBalance=this.accountBalance+amount;
+        return this.accountBalance;
+    }
+    get accnum(){
+        return this.#accountNumber;
+    }
+    set accnum(accno){
+        this.#accountNumber=accno;
+    }
+
+}// class body ended
+
+const account1=new BankAccount(); 
+console.log(account1);
+
+const account2=new BankAccount("salary");
+console.log(account2);
+
+const account3=new BankAccount("current", 100000); 
+console.log(account3);
+
+const account4=new BankAccount("salary", 200000, 'sc1234'); 
+console.log(account4);
+
+const account5=new BankAccount("salary", 200000, 'sc1235', 5454545454); 
+console.log(account5);
+let updatedBalance=account5.deposit(45000);
+console.log(updatedBalance);
+console.log(account5.accountBalance);
+
+const account6=new BankAccount(); // initialize
+console.log(account6);
+// state change
+account6.accnum=999999; // setter
+account6.customerId="SC4545";
+account6.accountBalance=78000;
+console.log(account6);
+console.log(account6.accnum); // getter
+
+
+
+ele.innerHTML="<b>hi</b>"; // setter
+console.log(ele.innerHTML); // getter
+
+
+
+// then use setters to changes default values
+
+
+
